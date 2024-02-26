77af6b84e763e31a3c7a1a8746d3a48c84c3ab3c
Miguel Sosa <msosadesign@gmail.com>
2024-02-26 23:16:25 +0100

    Updates

--- src/components/tokenContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-// A context that contains both tokens and a function to update each one
-export const TokenContext = createContext({
-  tokenState: { access: null, refresh: null },
-  setAccessToken: () => {},
-  setRefreshToken: () => {},
-});
